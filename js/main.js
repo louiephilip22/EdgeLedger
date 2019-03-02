70534b03c73d98ba5444059e106085623d15c3a6
Philip Louie Templonuevo <philiplouietemplonuevo@yahoo.ca>
2019-03-02 13:39:12 -0500

    Added responsiveness

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ $('#navbar a, .btn').on('click', function(event) {
     event.preventDefault();
     const hash = this.hash;
     $('html, body').animate({
-      scrollTop: $(hash).offset().top - 65
+      scrollTop: $(hash).offset().top - 100
     }, 800);
   }
 });
